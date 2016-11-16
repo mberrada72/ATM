a4c310b35b761f74c5ca3d2fc8e831198687cd89
mberrada72 <marouane.berrada@edu.dsti.institute>
2016-11-16 13:20:06 +0100

    ATM_Prototype

---
+++ ATM_Prototype/ATM_Prototype/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ATM_Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Validate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Validate
+            // 
+            this.Validate.Location = new System.Drawing.Point(552, 47);
+            this.Validate.Name = "Validate";
+            this.Validate.Size = new System.Drawing.Size(105, 35);
+            this.Validate.TabIndex = 1;
+            this.Validate.Text = "Validate";
+            this.Validate.UseVisualStyleBackColor = true;
+            this.Validate.Click += new System.EventHandler(this.Validate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 346);
+            this.Controls.Add(this.Validate);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Validate;
+    }
+}
+
---
+++ ATM_Prototype/ATM_Prototype/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bluelaser.Utilities;
+
+namespace ATM_Prototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Validate_Click(object sender, EventArgs e)
+        {
+            string cardNum = textBox1.Text;
+            if (CreditCardUtility.IsValidNumber(cardNum))
+            {
+                CreditCardTypeType? cardType = CreditCardUtility.GetCardTypeFromNumber(cardNum);
+                string strCardType = (cardType == null) ? "Unknown" : cardType.ToString();
+
+                MessageBox.Show("You have entered a valid card number. The card type is {0}." + strCardType);
+            }
+            else
+                MessageBox.Show("Card failed Luhn test. Please enter a valid card number.");
+        }
+    }
+}
